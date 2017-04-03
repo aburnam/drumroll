57ec895911cad0c8335f14229ca7e1382658e766
aburnam <burnam@live.unc.edu>
2017-04-02 22:34:33 -0400

    muh files
    woah

---
+++ Drumroll/site.js
@@ -0,0 +1,26 @@
+var mySound = null;
+// Fix up prefixing
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
+function loadSound(url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+
+  // Decode asynchronously
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      soundBuffer = buffer;
+    }, onError);
+  }
+  request.send();
+}
+
+function playSound(buffer) {
+  var source = context.createBufferSource(); // creates a sound source
+  source.buffer = buffer;                    // tell the source which sound to play
+  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+  source.start(0);                           // play the source now
+                                             // note: on older systems, may have to use deprecated noteOn(time);
+}
